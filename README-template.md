d61ad13d47e2beaba30e46a9c1583a5e61e42f87
abenezer-lab-code <abenezerforcode@gmail.com>
2026-03-13 21:58:15 +0300

    second  commit

--- README-template.md
+++ README-template.md
@@ -26,9 +26,9 @@ This is a solution to the [Browser extensions manager UI challenge on Frontend M
 Users should be able to:
 
 - Toggle extensions between active and inactive states
-- Filter active and inactive extensions
-- Remove extensions from the list
-- Select their color theme
+- Filter active and inactive extensions 
+- Remove extensions from the list 
+- Select their color theme [accessible button]([FireShot](https://getfireshot.com/)/)
 - View the optimal layout for the interface depending on their device's screen size
 - See hover and focus states for all interactive elements on the page
 
@@ -53,14 +53,10 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 
 ### Built with
 
-- Semantic HTML5 markup
+- Semantic HTML5 markup for accessibility and readibility
 - CSS custom properties
 - Flexbox
-- CSS Grid
-- Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
+- 
 
 **Note: These are just examples. Delete this note and replace the list above with your own choices**
 
---
+++ script.js
@@ -0,0 +1,174 @@
+//get data from local storage , if not we will to get from JSON
+//display message when they want to remove extension
+// and save changes to local storage
+// select there theme color and save to local storage
+const extensionContainer = document.querySelector(".extensions-container");
+const bodyElement = document.querySelector("body");
+const logoSection = document.querySelector(".logo-section");
+const extensionListHeading = document.querySelector(".extension-list-heading");
+const extensionFilterBtn = document.querySelectorAll(".extension-btn");
+
+
+
+const getExtesnionsFromJSON = async function(){
+    try{
+const response = await fetch("data.json");
+const extensionData= await response.json();
+localStorage.setItem("extensions",JSON.stringify(extensionData))
+console.log(extensionData)
+    }
+catch(error){
+console.log(error)
+
+    }
+
+
+}
+
+const populatePage = (page,data)=>
+{
+
+page.innerHTML=""
+data.forEach(({logo,name,description,isActive})=>{
+page.innerHTML+=`
+ <div class="extension" id="${name}">
+    <div class="image-text-container">
+      <img src="${logo}" class="logo-image" aria-hidden="true" loading="lazy"/>
+ <div class="extesnions-heading-and-text">
+<h3 class="extensions-heading">${name}</h3>
+  <P class="extension-text">${description}</P>
+  </div>
+  </div>
+  <div class="btn-and-input-container">
+ <button class="remove-btn">Remove</button>
+ <button class="on-and-of-extension-btn ${isActive}" aria-label="remove this extension">
+
+<span class="toggle-background"></span>
+ </button>
+ </div>
+</div>
+
+
+`
+
+})
+
+
+}
+
+const retriveExtensionsFromLocalStorage = ()=>{
+const extensions = localStorage.getItem("extensions");
+if(extensions!==null){
+return JSON.parse(extensions)
+}
+return undefined
+
+}
+
+const filterEextensions  = (extensionType,array)=>{
+
+if(extensionType==="inactive"){
+return array.filter(e=>e.isActive===false)
+}
+else if(extensionType==="active"){
+return array.filter(e=>e.isActive===true)
+}
+else{
+
+    return array
+}
+}
+
+
+const renderToHtml = (type)=>{
+
+
+const extensions = retriveExtensionsFromLocalStorage();
+
+
+if(extensions !== undefined){
+ const filtered = filterEextensions(type,extensions)
+   populatePage( extensionContainer,filtered)
+   return
+}
+
+else{
+
+getExtesnionsFromJSON()
+return renderToHtml()
+
+}
+} 
+renderToHtml()
+
+
+//filter 
+
+
+//remove 
+const removeExtension = (id)=>{
+const extensions = retriveExtensionsFromLocalStorage();
+
+const name = extensions.findIndex(e=>e.name===id);
+extensions.splice(name,1);
+localStorage.setItem("extensions",JSON.stringify(extensions))
+const element = document.getElementById(id);
+
+element.remove()
+}
+
+extensionContainer.addEventListener("click",e=>{
+
+if(e.target.className==="remove-btn"){
+console.log(e.target.parentElement.parentElement)
+removeExtension(e.target.parentElement.parentElement.id)
+
+}
+if(e.target.classList.contains("on-and-of-extension-btn")){
+const extensions = retriveExtensionsFromLocalStorage();
+const id = e.target.parentElement.parentElement.id;
+
+const indexOfModied = extensions.findIndex(e=>e.name===id)
+ const current =   [...extensionFilterBtn].filter(e=>e.classList.contains("current-btn"))[0].id;
+ 
+if(e.target.classList.contains("true")){
+
+e.target.classList.remove("true")
+e.target.classList.add("false")
+
+extensions[indexOfModied].isActive = false;
+localStorage.setItem("extensions",JSON.stringify(extensions))
+ 
+  renderToHtml(current)
+}
+else{
+e.target.classList.remove("false")
+e.target.classList.add("true") 
+extensions[indexOfModied].isActive = true;
+localStorage.setItem("extensions",JSON.stringify(extensions))
+renderToHtml(current)
+}
+}
+
+})
+extensionFilterBtn.forEach(btn=>{
+    btn.addEventListener("click",(e)=>{
+
+renderToHtml(btn.id)
+
+extensionFilterBtn.forEach(e=>{
+  e.classList.remove("current-btn")
+})
+btn.classList.add("current-btn")
+
+    })
+})
+
+
+
+
+
+
+
+
+
